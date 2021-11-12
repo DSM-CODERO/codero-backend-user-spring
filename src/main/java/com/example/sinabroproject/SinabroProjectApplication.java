23a2c9344a64dddde2118153f67dfb5d7072dfdc
heesung33 <chaeheesung33@gmail.com>
2021-11-12 19:08:34 +0900

    [CREATE]  repository
    기본 파일 생성

---
+++ src/main/java/com/example/sinabroproject/SinabroProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.sinabroproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinabroProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SinabroProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sinabroproject/SinabroProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sinabroproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinabroProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
