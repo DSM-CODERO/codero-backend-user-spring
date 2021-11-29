f35e7859662326f092f865c17ba6c485c1b2cc42
heesung33 <chaeheesung33@gmail.com>
2021-11-29 22:03:05 +0900

    [CREATE] User

--- src/main/java/com/example/sinabroproject/entity/user/entity/User.java
+++ src/main/java/com/example/sinabroproject/entity/user/entity/User.java
@@ -1,28 +1,94 @@
 package com.example.sinabroproject.entity.user.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
+
+
+import com.example.sinabroproject.entity.role.Role;
+import com.example.sinabroproject.entity.user.ERole;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
 
-@Builder
+
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
-@Table(name = "user")
+@Table
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
-    @Column(unique = true, length = 60)
+    @NotBlank
+    @Size(max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(max = 120)
     private String password;
 
-    @Column(unique = true, length = 10)
-    private String nickname;
+    @Enumerated(EnumType.STRING)
+    private ERole role;
 
-    @Column(unique = true, length = 36)
-    private String email;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(	name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    public User() {
+    }
+
+    public User(String username, String email, String password, ERole role) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
