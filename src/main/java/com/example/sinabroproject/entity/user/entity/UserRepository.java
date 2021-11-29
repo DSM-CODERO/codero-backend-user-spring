aa0ed25fd02e8cb377b8b87546c193ef19a5e80f
heesung33 <chaeheesung33@gmail.com>
2021-11-29 22:04:28 +0900

    [CREATE] UserRepository

--- src/main/java/com/example/sinabroproject/entity/user/entity/UserRepository.java
+++ src/main/java/com/example/sinabroproject/entity/user/entity/UserRepository.java
@@ -4,6 +4,13 @@ import com.example.sinabroproject.entity.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
 }
