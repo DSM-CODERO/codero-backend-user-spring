18d2fa262c79ddb92a344d0bd42bb257456fcd6a
heesung33 <chaeheesung33@gmail.com>
2021-11-29 22:01:28 +0900

    [CREATE] Role

---
+++ src/main/java/com/example/sinabroproject/entity/role/Role.java
@@ -0,0 +1,48 @@
+package com.example.sinabroproject.entity.role;
+
+import com.example.sinabroproject.entity.user.ERole;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+    public Role() {
+
+    }
+
+    public Role(ERole name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public ERole getName() {
+        return name;
+    }
+
+    public void setName(ERole name) {
+        this.name = name;
+    }
+}
