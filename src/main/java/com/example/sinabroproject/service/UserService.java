b2046e49f7801d5c84009168a2c6a5520b8555ac
heesung33 <chaeheesung33@gmail.com>
2021-11-29 18:05:21 +0900

    [DELETE] UserService

--- src/main/java/com/example/sinabroproject/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.example.sinabroproject.service;
-
-import com.example.sinabroproject.payload.request.UserRequest;
-
-public interface UserService {
-    String UserSignUp(UserRequest request);
-}
