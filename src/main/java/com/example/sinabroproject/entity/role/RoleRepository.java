bfa3a530ff6d3a4f7ba24a4e2e3324d73028a132
heesung33 <chaeheesung33@gmail.com>
2021-11-29 22:00:27 +0900

    [CREATE] RoleRepository

---
+++ src/main/java/com/example/sinabroproject/entity/role/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.example.sinabroproject.entity.role;
+
+import com.example.sinabroproject.entity.user.ERole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+}
