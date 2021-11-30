41374282edf94f74432bb9f1acd290a9bb0e90b4
heesung33 <chaeheesung33@gmail.com>
2021-11-30 19:03:27 +0900

    [UPDATE] MailController

--- src/main/java/com/example/sinabroproject/controller/email/MailController.java
+++ src/main/java/com/example/sinabroproject/controller/email/MailController.java
@@ -1,2 +1,24 @@
-package com.example.sinabroproject.controller.email;public class MailController {
+package com.example.sinabroproject.controller.email;
+
+import com.example.sinabroproject.service.mail.MailService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@ResponseBody
+@RestController
+public class MailController {
+
+    private final MailService mailService;
+
+    public MailController(MailService mailService) {
+        this.mailService = mailService;
+    }
+
+    @GetMapping("email")
+    @ResponseBody
+    public String SendMail(@RequestParam String email) {
+        return mailService.SendMail(email);
+    }
 }
