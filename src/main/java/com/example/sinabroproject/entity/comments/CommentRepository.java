333e7dd4955771d452311b7fc6675d4a18a8f228
heesung33 <chaeheesung33@gmail.com>
2021-12-06 23:28:08 +0900

    [DELETE] Comment

--- src/main/java/com/example/sinabroproject/entity/comments/CommentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.sinabroproject.entity.comments;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommentRepository extends JpaRepository<Comment,Integer> {
-}
