2a34e53067531b2140a9da3469b19c89dc8924d6
heesung33 <chaeheesung33@gmail.com>
2021-12-08 08:31:04 +0900

    [UPDATE] User

--- src/main/java/com/example/sinabroproject/entity/user/User.java
+++ src/main/java/com/example/sinabroproject/entity/user/User.java
@@ -40,7 +40,7 @@ public class User {
 
     @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(	name = "user_roles",
-            joinColumns = @JoinColumn(name = "user_id"),
+            joinColumns = @JoinColumn(name = "users_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
